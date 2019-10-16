f9a81cd7a420be88cc841c9570c78defafa7323f
emersonmeh <53608140+emersonmeh@users.noreply.github.com>
2019-10-16 08:00:39 -0300

    Add files via upload

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,23 @@
+<<<<<<< HEAD
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'/>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet ....</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet ....</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet ....</p>
+    </div>
+=======
 import React from 'react'
 
 export default props => (
     <div>
         <h1>Sobre</h1>
     </div>
+>>>>>>> 8a359922aac2a0b36d93645bfbd9e97deeb16ab5
 )
